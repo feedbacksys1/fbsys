e2a7562915dc4a77d2361d8e56f44fb5d5d0bb04
feedbacksys1 <feedbacksys@kolabnow.com>
2026-02-12 15:18:58 +0400

    Changed redirect logic after registration

--- app/views.py
+++ app/views.py
@@ -60,8 +60,9 @@ def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('site_login')
+            user = form.save()
+            login(request, user)
+            return redirect('landing')
     else:
         form = RegistrationForm()
     return render(request, 'register.html', {'form': form})

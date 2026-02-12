5698c778994cfd4f2dc7e9ee14ce4481e99449bd
feedbacksys1 <feedbacksys@kolabnow.com>
2026-02-12 14:59:42 +0400

    Added view for authorized users

--- app/urls.py
+++ app/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from . import views
@@ -25,5 +26,6 @@ urlpatterns = [
     path('feedback/', views.feedback, name='feedback'),
     path('login/', views.login_view, name='site_login'),
     path('register/', views.register, name='site_register'),
+    path('logout/', LogoutView.as_view(next_page='landing'), name='site_logout'),
     path('admin/', admin.site.urls),
 ]
